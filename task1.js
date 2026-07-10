6bf10a9bfcad3f8a171a68f302909c2743b76630
Anup Kumal <morxffxz@gmail.com>
2026-07-10 13:02:14 +0545

    FEWO-10 removing the commments

--- task1.js
+++ task1.js
@@ -1,22 +1,3 @@
-// console.log("Time");
-// function printCurrentDateTime() {
-//   const date = new Date();
-//   console.log(date);
-// }
-
-// function startTimer(interval, durations, callback) {
-//   const timer = setInterval(() => {
-//     callback();
-//   }, interval);
-
-//   setTimeout(() => {
-//     clearInterval(timer);
-//     console.log("Operation Finished");
-//   }, durations);
-// }
-
-// startTimer(1000, 5000, printCurrentDateTime);
-
 console.log("Time");
 
 function printCurrentDateTime() {
